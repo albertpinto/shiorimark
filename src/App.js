e4481e48450595d88a5298f086241223cd6bca0a
albertpinto123@gmail.com <albertpinto123@gmail.com>
2020-03-17 21:12:11 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    status: "Login"
+  };
+
+  switchNameHandler = () => {
+    console.log(this.state.status);
+    if (this.state.status === "Logout") {
+      console.log("Was clicked Logout..");
+      this.setState({
+        status: "Login"
+      });
+    } else {
+      console.log("Was clicked login..");
+      this.setState({
+        status: "Logout"
+      });
+    }
+  };
+  render() {
+    return (
+      <div>
+      <Navbar appName ="Shirorimark" userName ="Albert Pinto" status={this.state.status} />       
+      <button onClick={this.switchNameHandler}>SwitchName</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+      // <Route path="/bookmarks" component={Bookmarks} />
+            // <Route path="/categories" component={Categories} />
+            // <Route path="/users" component={Users} />
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+const navbar = (props) => {
+  return (<div>
+    This is navbar {props.status}
+    </div>
+  );
+};
+export default navbar;
