e3b3ebb794e5ad62d7b0a67e525154a9091132a1
albertpinto123@gmail.com <albertpinto123@gmail.com>
2020-04-08 18:16:14 -0700

    Basic login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,62 @@
-import React from "react";
+import React from 'react'
+import { render } from 'react-dom'
+import Styles from './Styles'
+import { Form, Field } from 'react-final-form'
 
-const login = (props) => {
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
+}
+
+
+const login = () => {
   return (
-    <section id="login">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 mx-auto">
-            <div className="card">
-              <div className="card-header">
-                <h4>Account Login</h4>
-              </div>
-              <div className="card-body">
-                <form action="index.html">
-                  <div className="form-group">
-                    <label for="email">Email</label>
-                    <input type="text" className="form-control"></input>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <Form onSubmit={onSubmit}>
+            {({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="card">
+                  <div className="card-header">
+                    <h4>Account Login</h4>
                   </div>
-                  <div className="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" className="form-control"></input>
+                  <div className="card-body">
+                    <div className="form-group">
+                      <label>First Name</label>
+                      <Field
+                        name="firstName"
+                        component="input"
+                        placeholder="First Name"
+                        className="form-control"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Last Name</label>
+                      <Field
+                        name="lastName"
+                        component="input"
+                        placeholder="Last Name"
+                        className="form-control"
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Login
+                    </button>
                   </div>
-                  <input
-                    type="submit"
-                    value="Login"
-                    className="btn btn-primary btn-block"
-                  ></input>
-                </form>
-              </div>
-            </div>
-          </div>
+                </div>
+              </form>
+            )}
+          </Form>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
-export default login;
+  
+
+
+  export default login;
