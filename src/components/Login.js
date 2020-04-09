549fc82e1b3af981da7c75b7977fd7e994cf5d98
albertpinto123@gmail.com <albertpinto123@gmail.com>
2020-04-08 20:48:02 -0700

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,13 @@ import { render } from 'react-dom'
 import Styles from './Styles'
 import { Form, Field } from 'react-final-form'
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
+//const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const required =value =>(value ?undefined:"Required")
 const onSubmit = async values => {
-  await sleep(300)
+  //await sleep(300)
   window.alert(JSON.stringify(values, 0, 2))
 }
-
 
 const login = () => {
   return (
@@ -17,32 +17,62 @@ const login = () => {
       <div className="row">
         <div className="col-md-6 mx-auto">
           <Form onSubmit={onSubmit}>
-            {({ handleSubmit }) => (
+            {({ handleSubmit, submitting, values }) => (
               <form onSubmit={handleSubmit}>
                 <div className="card">
                   <div className="card-header">
                     <h4>Account Login</h4>
                   </div>
                   <div className="card-body">
-                    <div className="form-group">
-                      <label>First Name</label>
-                      <Field
-                        name="firstName"
-                        component="input"
-                        placeholder="First Name"
-                        className="form-control"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label>Last Name</label>
-                      <Field
-                        name="lastName"
-                        component="input"
-                        placeholder="Last Name"
-                        className="form-control"
-                      />
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-block">
+                    <Field
+                      name="email"
+                      component="input"
+                      placeholder="Email"
+                      className="form-control"
+                      validate={required}
+                    >
+                      {({ input, meta, placeholder }) => (
+                        <div className="form-group">
+                          <label>Email</label>
+                          <input
+                            {...input}
+                            className="form-control"
+                            type="text"
+                            placeholder={placeholder}
+                          />
+                          {meta.error && meta.touched && (
+                            <span>{meta.error}</span>
+                          )}
+                        </div>
+                      )}
+                    </Field>
+                    <Field
+                      name="password"
+                      component="input"
+                      placeholder="Password"
+                      className="form-control"
+                      validate={required}
+                    >
+                      {({ input, meta, placeholder }) => (
+                        <div className="form-group">
+                          <label>Password</label>
+                          <input
+                            {...input}
+                            type="password"
+                            className="form-control"
+                            placeholder={placeholder}
+                          />
+                          {meta.error && meta.touched && (
+                            <span>{meta.error}</span>
+                          )}
+                        </div>
+                      )}
+                    </Field>
+                    <button
+                      type="submit"
+                      disabled={submitting}
+                      className="btn btn-primary btn-block"
+                    >
                       Login
                     </button>
                   </div>
